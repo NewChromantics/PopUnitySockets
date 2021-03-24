874872024fdb3e67270ca21c1b8c1f83490ad5cf
Graham Reeves <graham@grahamreeves.com>
2021-03-24 14:51:42 +0000

    More clock alternatives & notes

--- PopFrameCounter.cs
+++ PopFrameCounter.cs
@@ -18,15 +18,23 @@ namespace PopX
 		//	we don't use Time.time as that can only be used on the main thread
 		//	UtcNow is supposedly faster https://stackoverflow.com/a/1561894/355753
 		//	and we just need a consistent time reference, not an "accurate" time
-		//float NowSecs { get { return (float)System.DateTime.Now.Subtract(StartTime).TotalSeconds; } }
-		float NowSecs { get { return (float)System.DateTime.UtcNow.Subtract(StartTime).TotalSeconds; } }
+		//	stopwatch and UTCNow seem to be on par with cost (tested against 80,000 calls per sec)
 
+		//float NowSecs { get { return (float)System.DateTime.Now.Subtract(InitTime).TotalSeconds; } }
+		//float NowSecs { get { return (float)System.DateTime.UtcNow.Subtract(InitTime).TotalSeconds; } }	//	subtract() to get a timespan in order to get total
+		float NowSecs { get { return Stopwatch.ElapsedMilliseconds / 1000.0f; } } 
+		//float NowSecs { get { return (float)UnityEngine.Time.time; } }
+		//float NowSecs { get { return (float)UnityEngine.Time.realtimeSinceStartup; } }
+
+		//	initialisation variables for different clocks above
+		System.Diagnostics.Stopwatch Stopwatch = System.Diagnostics.Stopwatch.StartNew();
+		System.DateTime InitTime = System.DateTime.UtcNow;	//	this can be any datetime value, just needs to be fixed. Clock comparison is done via LastLapTime.
+		
 		float Counter = 0;
 		float LastLapTime;
 		float ReportEverySeconds;
 		System.Action<float> OnSecondLapsed;
 
-		System.DateTime StartTime = System.DateTime.UtcNow;
 
 		public FrameCounter(System.Action<float> OnSecondLapsed, float ReportEverySeconds = 1)
 		{
